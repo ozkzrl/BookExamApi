21f137f9be33c359bc03984a50020b1ebbe88074
ozkzrl <ozkan.zorlu@gmail.com>
2025-03-08 22:58:55 +0300

    Emtities were updated.

--- BookExam.Domain/Entities/teacher.cs
+++
@@ -1,8 +0,0 @@
-public class Teacher
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-
-    // Öğretmenin birden fazla öğrencisi olabilir
-    public List<Student> Students { get; set; }
-}
